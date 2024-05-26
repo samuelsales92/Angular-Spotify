1b2b7b7a5d561eb61d26756e8b4f2e9667c6303a
samuelsales92 <samuelsales1410@gmail.com>
2024-05-25 23:42:30 -0300

    commit

--- Angular-Spotify/src/app/app.module.ts
+++ Angular-Spotify/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
   ],
   providers: [
     provideClientHydration()
