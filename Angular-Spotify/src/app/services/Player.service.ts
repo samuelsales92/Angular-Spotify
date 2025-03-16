b1cc5bd87fc0c6963c7a7f3029f3c14ecd7ef1e3
samuelsales92 <samuelsales1410@gmail.com>
2025-03-16 15:37:45 -0300

    up

--- Angular-Spotify/src/app/services/Player.service.ts
+++ Angular-Spotify/src/app/services/Player.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import { IMusica } from '../Interfaces/IMusica';
+import { newMusica } from '../Common/factories';
+import { BehaviorSubject} from 'rxjs';
+import { SpotifyService } from './Spotify.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-constructor() { }
+musicaAtual = new BehaviorSubject<IMusica>(newMusica());
+
+constructor(private spotifyService: SpotifyService) {
+  this.obterMusicaAtual();
+ }
+
+
+
+async obterMusicaAtual(){
+  const musica = await this.spotifyService.obterMusicaAtual();
+  this.musicaAtual.next(musica);
+  }
+
+definirMusicaAtual(){
+  
+}
 
 }
