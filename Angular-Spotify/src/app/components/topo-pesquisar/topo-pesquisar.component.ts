97fe420a128c67b3f53cf83154a11a591cc752a7
samuelsales92 <samuelsales1410@gmail.com>
2025-03-26 21:21:03 -0300

    atualizando pesquisar

--- Angular-Spotify/src/app/components/topo-pesquisar/topo-pesquisar.component.ts
+++ Angular-Spotify/src/app/components/topo-pesquisar/topo-pesquisar.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../../services/Spotify.service';
 import { PlayerService } from '../../services/Player.service';
 import { promises } from 'dns';
+import { set } from 'date-fns';
 
 @Component({
   selector: 'topo-pesquisar',
@@ -15,6 +16,7 @@ export class TopoPesquisarComponent implements OnInit {
 
   query: string = '';
   results: any[] = [];
+  timeout: any;
 
   constructor(
     private spotifyService: SpotifyService,
@@ -29,19 +31,24 @@ export class TopoPesquisarComponent implements OnInit {
 
   buscarPesquisa(e: Event) {
     const target = e.target as HTMLInputElement;
-    const value = target.value.trim();
+    const value = target.value.trim()
 
-    if (value.length < 4) return; // Evita buscas desnecessárias
+    if (value.length < 4 ) return; // Evita buscas desnecessárias
 
-    this.spotifyService.search(value, ['artist', 'track']).then(
-      (data) => {
-        console.log('Resultados:', data);
-        this.results = [...(data.tracks?.items || []), ...(data.artists?.items || [])];
-      },
-      (error) => {
-        console.error('Erro ao buscar no Spotify', error);
-      }
-    );
+    clearTimeout(this.timeout);
+
+    // Define um novo timeout para aguardar 500ms antes de buscar
+    this.timeout = setTimeout(() => {
+      this.spotifyService.search(value, ['artist', 'track']).then(
+        (data) => {
+          console.log('Resultados:', data);
+          this.results = [...(data.tracks?.items || []), ...(data.artists?.items || [])];
+        },
+        (error) => {
+          console.error('Erro ao buscar no Spotify', error);
+        }
+      );
+    }, 500); // Aguarda 500ms antes de executar a busca
   }
   
 }
