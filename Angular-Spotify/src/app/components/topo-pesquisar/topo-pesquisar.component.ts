37b4a3f3fe770a665dc7f389865c4a270d09d48a
samuelsales92 <samuelsales1410@gmail.com>
2025-03-26 21:05:08 -0300

    atualizando pesquisar

--- Angular-Spotify/src/app/components/topo-pesquisar/topo-pesquisar.component.ts
+++ Angular-Spotify/src/app/components/topo-pesquisar/topo-pesquisar.component.ts
@@ -1,4 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
+
+import { SpotifyService } from '../../services/Spotify.service';
+import { PlayerService } from '../../services/Player.service';
+import { promises } from 'dns';
 
 @Component({
   selector: 'topo-pesquisar',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopoPesquisarComponent implements OnInit {
 
-  constructor() { }
+
+  query: string = '';
+  results: any[] = [];
+
+  constructor(
+    private spotifyService: SpotifyService,
+    private playerService: PlayerService,
+
+  ) {  
+
+  }
 
   ngOnInit() {
   }
 
+  buscarPesquisa(e: Event) {
+    const target = e.target as HTMLInputElement;
+    const value = target.value.trim();
+
+    if (value.length < 4) return; // Evita buscas desnecessárias
+
+    this.spotifyService.search(value, ['artist', 'track']).then(
+      (data) => {
+        console.log('Resultados:', data);
+        this.results = [...(data.tracks?.items || []), ...(data.artists?.items || [])];
+      },
+      (error) => {
+        console.error('Erro ao buscar no Spotify', error);
+      }
+    );
+  }
+  
 }
--- Angular-Spotify/src/app/services/Spotify.service.ts
+++ Angular-Spotify/src/app/services/Spotify.service.ts
@@ -22,51 +22,55 @@ export class SpotifyService {
   spotifyApi: SpotifyWebApi.SpotifyWebApiJs;
   usuario: IUsuario | null = null;  // Inicializa como null
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+  ) {
     this.spotifyApi = new SpotifyWebApi();
-    
+
+
+
   }
 
-   async inicializarUsuario() {
-     if(!!this.usuario)
-       return true;
+  async inicializarUsuario() {
+    if (!!this.usuario)
+      return true;
 
-     const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token');
 
-     if(!token)
-       return false; 
+    if (!token)
+      return false;
 
-     try {
+    try {
 
-       this.definirAccessToken(token);
-       await this.obterSpotifyUsuario();
-       return !!this.usuario;
-      
+      this.definirAccessToken(token);
+      await this.obterSpotifyUsuario();
+      return !!this.usuario;
 
-     }catch(ex){
+
+    } catch (ex) {
       return false
-      
-     }
-   }
 
-   async obterSpotifyUsuario() {
-
-      const userInfo = await this.spotifyApi.getMe();
-      this.usuario = SpotifyUserParaUsuario(userInfo);
-    
     }
-      
-      
-      
-      // Obtém o perfil do usuário do Spotify
-  
-      // Mapeia os dados recebidos para o formato esperado por IUsuario
-     
-  
-  
-  
+  }
 
-  obterUrlLogin(){
+  async obterSpotifyUsuario() {
+
+    const userInfo = await this.spotifyApi.getMe();
+    this.usuario = SpotifyUserParaUsuario(userInfo);
+
+  }
+
+
+
+  // Obtém o perfil do usuário do Spotify
+
+  // Mapeia os dados recebidos para o formato esperado por IUsuario
+
+
+
+
+
+  obterUrlLogin() {
     const authEndipoint = `${SpotifyConfiguration.authEndpoint}?`;
     const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
     const redirectUrl = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
@@ -76,79 +80,83 @@ export class SpotifyService {
   };
 
   obterTokenUrlCallback(): string {
-      // Verifica se o código está sendo executado no lado do cliente
+    // Verifica se o código está sendo executado no lado do cliente
     if (typeof window === 'undefined' || !window.location.hash) {
       return '';
     }
-  
+
     // Divide o hash por '&' para obter os parâmetros
     const params = window.location.hash.substring(1).split('&');
     return params[0].split('=')[1];
-    
+
   }
 
-  definirAccessToken(token:string){
+  definirAccessToken(token: string) {
     this.spotifyApi.setAccessToken(token)
     localStorage.setItem('token', token);
-    
+
   }
 
 
-  
-  async buscarPlaylistUsuario(offset = 0, limit = 50): Promise<IPlaylist[]>{
-    const playlists = await this.spotifyApi.getUserPlaylists(this.usuario.id, { offset, limit});
+
+  async buscarPlaylistUsuario(offset = 0, limit = 50): Promise<IPlaylist[]> {
+    const playlists = await this.spotifyApi.getUserPlaylists(this.usuario.id, { offset, limit });
     return playlists.items.map(SpotifyPlaylistParaPlaylist);
-    
+
   }
 
 
-  async buscarTopArtistas(limit = 10 ):Promise<IArtista[]> {
+  async buscarTopArtistas(limit = 10): Promise<IArtista[]> {
     const artistas = await this.spotifyApi.getMyTopArtists({ limit });
     return artistas.items.map(SpotifyArtistaParaArtista);
   }
 
-  async buscarMusicas(offset = 0, limit = 50):Promise<IMusica[]>{
-    const musica = await this.spotifyApi.getMySavedTracks({ offset, limit});
-    return musica.items.map( x => SpotifyTrackParaMusica(x.track));
+  async buscarMusicas(offset = 0, limit = 50): Promise<IMusica[]> {
+    const musica = await this.spotifyApi.getMySavedTracks({ offset, limit });
+    return musica.items.map(x => SpotifyTrackParaMusica(x.track));
   }
 
- 
-async tocarMusica(musica: IMusica) {
-  try {
-    const dispositivos = await this.spotifyApi.getMyDevices();
-    if (!dispositivos.devices.length) {
-      console.warn('⚠️ Nenhum dispositivo ativo encontrado!');
-      return;
+
+  async tocarMusica(musica: IMusica) {
+    try {
+      const dispositivos = await this.spotifyApi.getMyDevices();
+      if (!dispositivos.devices.length) {
+        console.warn('⚠️ Nenhum dispositivo ativo encontrado!');
+        return;
+      }
+
+      const deviceId = dispositivos.devices[0].id;
+
+      await this.spotifyApi.play({
+        uris: [musica.uri],
+        device_id: deviceId
+      });
+
+    } catch (error) {
+      console.error('❌ Erro ao tentar tocar a música:', error);
     }
-
-    const deviceId = dispositivos.devices[0].id; 
-
-    await this.spotifyApi.play({
-      uris: [musica.uri],
-      device_id: deviceId 
-    });
-
-  } catch (error) {
-    console.error('❌ Erro ao tentar tocar a música:', error);
   }
-}
 
 
-async obterMusicaAtual(): Promise<IMusica> {
-  const musicaSpotify = await this.spotifyApi.getMyCurrentPlayingTrack();
-  return SpotifyTrackParaMusica(musicaSpotify.item);
-}
+  async obterMusicaAtual(): Promise<IMusica> {
+    const musicaSpotify = await this.spotifyApi.getMyCurrentPlayingTrack();
+    return SpotifyTrackParaMusica(musicaSpotify.item);
+  }
 
 
-  
-  logout(){
+
+  logout() {
     localStorage.clear();
     this.router.navigate(['/login']);
   }
 
-  
-  
+  search(query: string, type: ('album' | 'artist' | 'playlist' | 'track')[]) {
+    return this.spotifyApi.search(query, type);
+  }
 }
+
+
+
 
 
 
