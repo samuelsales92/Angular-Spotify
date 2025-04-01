63d5aa6583f9d8f52ca29d30cb166487282b718f
samuelsales92 <samuelsales1410@gmail.com>
2025-04-01 18:34:39 -0300

    component pesquisar

--- Angular-Spotify/src/app/Common/spotifyHelper.ts
+++ Angular-Spotify/src/app/Common/spotifyHelper.ts
@@ -10,9 +10,9 @@ import { IUsuario } from "../Interfaces/IUsuario";
 
 export function SpotifyUserParaUsuario(user: SpotifyApi.CurrentUsersProfileResponse): IUsuario {
     return {
-        id: user.id || 'id padrão', // Atribui o id do usuário
-        nome: user.display_name || 'Usuário sem nome', // Nome do usuário
-        imagemUrl: user.images && user.images.length > 0 ? user.images[0].url : 'URL da imagem padrão', // URL da imagem
+        id: user.id || 'id padrão', 
+        nome: user.display_name || 'Usuário sem nome', 
+        imagemUrl: user.images && user.images.length > 0 ? user.images[0].url : 'URL da imagem padrão', 
     }
 }
 
